c412c4b487976e8211c1ec2408a5d41eba1b9188
Prashant <prashantco111@gmail.com>
2024-05-08 09:07:56 +0530

    Add postcss.config.js, tailwind.config.js, and src/assets/icon/UpArrow.svelte Update src/main.ts, package.json, src/lib/PromptInput.svelte, and tsconfig.json Delete src/lib/Counter.svelte Refactor src/app.css and src/App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,15 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+<script>
+	import PromptInput from "./lib/PromptInput.svelte"
+
 </script>
-
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<div class="bg-gray-800 w-full h-screen overflow-hidden py-4">
+  <div class="w-[95%] sm:w-[90%] md:w-[70%] xl:w-[50%] 2xl:w-[40%] 3xl:w-[30%] m-auto flex flex-col">
+    <!-- Visible field -->
+    <div class="grow">
+      <div class="bg-gray-900 p-4 rounded-lg">
+        <p class="text-white">Hello, world!</p>
+      </div>
+    </div>
+    <PromptInput />
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+</div>
---
+++ src/lib/PromptInput.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import UpArrow from "../assets/icon/UpArrow.svelte"
+
+  export let isProcessing:boolean = false
+</script>
+
+<div class="border border-gray-400 bg-transparent text-white py-4 px-6 rounded-xl flex flex-row">
+  <input class="focus:bg-transparent focus:outline-none w-full h-full bg-transparent" disabled={isProcessing} />
+  <div class="h-3 w-3 border-2 p-2 rounded-md">
+    <!-- <UpArrow  /> -->
+  </div>
+</div>
